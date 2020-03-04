b569bf4952f350330d7725d74ee4c5cdf7f2344f
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-03-04 02:45:17 -0500

    added the rest of the information to schedule; array for states in index

--- index.php
+++ index.php
@@ -18,6 +18,19 @@
 
     $controller = new Controller($f3);
 
+
+$f3->set('states', array('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Colorado', 'Connecticut','Delaware','District Of Columbia', 'Florida',
+    'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
+    'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+    'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+    'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York' ,'North Carolina', 'North Dakota',
+    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah' , 'Vermont',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'));
+
+
     //Define a default route
     $f3->route('GET /', function (){
         $GLOBALS['controller']->home();
